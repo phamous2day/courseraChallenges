c874d5205fb302060f074a8926b7827a441b3b68
David Pham <phamous2day@users.noreply.github.com>
2016-10-19 01:07:27 -0400

    Create setsNintersections.py

---
+++ UdemyPythonPostgreSQL/setsNintersections.py
@@ -0,0 +1,6 @@
+lottery_values = {3,5,17,8}
+user_values = {3,2,11,5}
+
+print (lottery_values.intersection(user_values))
+
+#returns intersecting values from both sets/list of numbers in each variable
