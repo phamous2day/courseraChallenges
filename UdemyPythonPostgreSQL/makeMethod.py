541cc07dd0920e64a2450232b1ffcf42a058a927
David Pham <phamous2day@users.noreply.github.com>
2016-10-18 21:16:26 -0400

    Update makeMethod.py
    Made another method to run program dependent upon the amount of times user wants program to run.

--- UdemyPythonPostgreSQL/makeMethod.py
+++ UdemyPythonPostgreSQL/makeMethod.py
@@ -8,7 +8,13 @@ def ask_user_and_check_number():
         print("You totally got the number WRONG!")
 
 magic_numbers = [random.randint(0,9), random.randint(0,9)]
-chances = 3
-for attempt in range(chances): #range(changes) is [0,1,2]
-    print("This is attempt{}".format(attempt))
-    ask_user_and_check_number()
+
+
+
+def run_program_x_times(chances):
+    for attempt in range(chances): #range(changes) is [0,1,2]
+        print("This is attempt{}".format(attempt))
+        ask_user_and_check_number()
+
+user_attempts = int(input("Enter number of attempts:  "))
+run_program_x_times(user_attempts)
